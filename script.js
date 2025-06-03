d2e4aa258114a0eb59dda490a7246c8ebe30582c
Thuranius <corey.gatter@gmail.com>
2025-06-02 18:38:25 -0700

    Built pages and added imagines.

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('thingsToDo').addEventListener('click', e => {
+  window.location.href = 'todo.html'
+})
+
+document.getElementById('placesToGo').addEventListener('click', e => {
+  window.location.href = 'togo.html'
+})
+
+document.getElementById('getAround').addEventListener('click', e => {
+  window.location.href = 'getAround.html'
+})
